5be233392bf091a7a58a25868152bd52c4abc928
Emiliano Gandini Outeda <emiliano.gandini@protonmail.com>
2026-09-12 20:40:23 -0300

    chore: bump to 0.4.0

--- src/detrack/__init__.py
+++ src/detrack/__init__.py
@@ -15,7 +15,7 @@ from detrack.core import DetrackResult, clean, clean_batch, clean_query, clean_u
 from detrack.patterns import DEFAULT_PATTERNS, PREFIXES
 from detrack.settings import DEFAULT_SETTINGS, Settings, configure
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 __all__ = [
     "clean",
